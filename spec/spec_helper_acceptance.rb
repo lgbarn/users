67a81ce907a582f6765166c3224f0b9e01860fae
Luther Barnum <luther_barnum@jabil.com>
2015-04-10 14:11:47 -0400

    created skeleton for spec and acceptance testing

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,28 @@
+require 'beaker-rspec'
+
+UNSUPPORTED_PLATFORMS = [ 'Windows', 'Solaris', 'AIX' ]
+
+  hosts.each do |host|
+    install_puppet
+  end
+
+  hosts.each do |host|
+    on hosts, "mkdir -p #{host['distmoduledir']}"
+  end
+
+RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    # Install module and dependencies
+    puppet_module_install(:source => proj_root, :module_name => 'users', :target_module_path => '/etc/puppet/modules/')
+    hosts.each do |host|
+      on host, puppet('module install puppetlabs-stdlib -i /etc/puppet/modules/'), { :acceptable_exit_codes => [0,1] }
+    end
+  end
+end
